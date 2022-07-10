092de285468d62fb6b6b9cca9870006389706f5a
Andy-Fu <fua3530@gmail.com>
2022-07-10 21:10:04 +0800

    老虎机小游戏-新增余额显示

--- com/andy/testdemo1/DiceGame.py
+++ com/andy/testdemo1/DiceGame.py
@@ -12,6 +12,7 @@ button:tkinter.Button
 input:tkinter.Entry
 stake:tkinter.Entry
 deposit:tkinter.Entry
+balanceVar:tkinter.StringVar
 inputvar :tkinter.StringVar
 stakeVar :tkinter.StringVar
 depositVar :tkinter.StringVar
@@ -69,12 +70,13 @@ def getStakeValue():
 
 def getDepositValue():
     print("getDepositValue")
-    global deposit,depositVar,totleMoney
+    global deposit,depositVar,totleMoney,balanceVar
     depositValue = deposit.get()
     try:
         depositValue = int(depositValue)
         totleMoney+=depositValue
         print(f"充值成功，账户余额：{totleMoney}")
+        balanceVar.set("账户余额："+str(totleMoney))
         deposit.delete(0, "end")
         return True
     except Exception as e:
@@ -83,7 +85,7 @@ def getDepositValue():
         return False
 
 def clickStart():
-    global input,stake,totleMoney,button
+    global input,stake,totleMoney,button,balanceVar
     inputValue = input.get()
     stakeValue = stake.get()
     button.focus_force()
@@ -102,32 +104,40 @@ def clickStart():
     else:
         totleMoney -= stakeValue
         print(f"很抱歉！{totleMoney}")
+    balanceVar.set("账户余额：" + str(totleMoney))
 
 
 
 
 
 def mainView():
-    global input,stake,deposit,depositVar,inputvar,stakeVar,button
+    global input,stake,deposit,depositVar,inputvar,stakeVar,button,totleMoney,balanceVar
     window = tkinter.Tk()
+    window.geometry("600x300")
     inputvar = tkinter.StringVar()
     stakeVar = tkinter.StringVar()
     depositVar = tkinter.StringVar()
-    lable = tkinter.Label(window,text="请猜大小(大/小)：")
-    input = tkinter.Entry(window,textvariable=inputvar,validate="focusout",  validatecommand=getInputValue)
-    lable1 = tkinter.Label(window, text="请输入赌注大小(正整数)：")
-    stake = tkinter.Entry(window,textvariable=stakeVar,validate="focusout", validatecommand = getStakeValue)
-    lable2 = tkinter.Label(window, text="请输入充值金额：")
-    deposit = tkinter.Entry(window,textvariable=depositVar,validate="focusout", validatecommand = getDepositValue)
-    button = tkinter.Button(window, text="开始游戏",command=clickStart)
+    balanceVar = tkinter.StringVar()
+    balanceVar.set("账户余额："+ str(totleMoney))
+    welcom = tkinter.Label(window,text="欢迎来到老虎机",width=20,height=5,font="黑体 18 bold",anchor="w")
+    balance = tkinter.Label(window,textvariable=balanceVar,width=20,height=5,font="黑体 12 bold",anchor="e")
+    lable = tkinter.Label(window,text="请猜大小(大/小)：",width=20)
+    input = tkinter.Entry(window,textvariable=inputvar,validate="focusout",width=20,  validatecommand=getInputValue)
+    lable1 = tkinter.Label(window, text="请输入赌注大小(正整数)：",width=20)
+    stake = tkinter.Entry(window,textvariable=stakeVar,validate="focusout",width=20, validatecommand = getStakeValue)
+    lable2 = tkinter.Label(window, text="请输入充值金额：",width=20)
+    deposit = tkinter.Entry(window,textvariable=depositVar,validate="focusout",width=20, validatecommand = getDepositValue)
+    button = tkinter.Button(window, text="开始游戏",width=40,background="pink",command=clickStart)
     input.delete(0,"end")
-    lable.grid(column=0,row=0)
-    input.grid(column=1,row=0)
-    lable1.grid(column=2, row=0)
-    stake.grid(column=3,row=0)
-    lable2.grid(column=0,row=1)
-    deposit.grid(column=1,row=1)
-    button.grid(column=2,row=1)
+    welcom.grid(column=0,columnspan=2,row=0,ipadx=0)
+    balance.grid(column=1,columnspan=2,row=0,ipadx=0)
+    lable.grid(column=0,row=1)
+    input.grid(column=1,row=1)
+    lable1.grid(column=2, row=1)
+    stake.grid(column=3,row=1)
+    lable2.grid(column=0,row=2)
+    deposit.grid(column=1,row=2)
+    button.grid(column=2,columnspan=2,row=2)
     window.mainloop()
 
 def mainRoot():
